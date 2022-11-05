91ba9acde872b00fc57f77931e43c7ecfa5a5d00
Stefan Visser <me@stefanvssr.dev>
2022-11-05 23:11:13 +0100

    Store and lowercase the input of the user

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let computerChoice;
-// let UserInput = prompt("Make a choice: Rock, Paper or Scissor")
 
+// Store the input of the user and make the input of the user lowercase
+let UserChoice = prompt("Make your choice: Rock, Paper or Scissor").toLowerCase();
+console.log(UserChoice);
+
+// The computer randomly makes a choice between rock, paper or scissors
 function getComputerChoice() {
     const computerChoices = ["rock", "paper", "scissors"];
     return computerChoices[Math.floor(Math.random() * computerChoices.length)];
 }
 
+// Store the selection of the computer
 computerChoice = getComputerChoice();
-
