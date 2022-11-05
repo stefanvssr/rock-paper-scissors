2f0e500ded05fc3848ca753dd7181a9c6c42067b
Stefan Visser <me@stefanvssr.dev>
2022-11-05 23:44:01 +0100

    Check if the player beats the computer
    The function will check if the player beats the computer

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let computerChoice;
 
 // Store the input of the user and make the input of the user lowercase
-let UserChoice = prompt("Make your choice: Rock, Paper or Scissor").toLowerCase();
-console.log(UserChoice);
+let userChoice = prompt("Make your choice: Rock, Paper or Scissor").toLowerCase();
 
 // The computer randomly makes a choice between rock, paper or scissors
 function getComputerChoice() {
@@ -12,3 +11,30 @@ function getComputerChoice() {
 
 // Store the selection of the computer
 computerChoice = getComputerChoice();
+console.log(computerChoice);
+
+// We will check if the playerSelection (userChoice) beats the computerSelection (computerChoice)
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Win! Rock beats Scissors";
+
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You Lose! Paper beats Rock";
+
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win! Paper beats Rock";
+
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You Lose! Scissors beats Paper";
+
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! Scissors beats Paper";
+
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You Lose! Rock beats Scissors";
+    } else {
+        return "Error! You didn't choose Rock, Paper or Scissors";
+    }
+}
+
+console.log(playRound(userChoice, computerChoice));
