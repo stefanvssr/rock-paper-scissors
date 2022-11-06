10e620d235102e5f4580772266688c7a4bd5c8e0
Stefan Visser <me@stefanvssr.dev>
2022-11-06 21:52:05 +0100

    Finish v1.0 of the game

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let computerChoice;
 
+// Scores
+let userScore = 0;
+let computerScore = 0;
+
 // The computer randomly makes a choice between rock, paper or scissors
 function getComputerChoice() {
     const computerChoices = ["rock", "paper", "scissors"];
@@ -8,35 +12,37 @@ function getComputerChoice() {
 
 // We will check if the playerSelection (userChoice) beats the computerSelection (computerChoice)
 function playRound(playerSelection, computerSelection) {
+    // Check if player beats the computer and will return a array with a string and a number. We will use the number in the game() function to check if the player or the computer gets points
+    // 0 player lose, 1 player win, 2 draw nobody wins or if you make a typo
     if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You Win! Rock beats Scissors";
+        return ["You Win! Rock beats Scissors", 1];
 
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You Lose! Paper beats Rock";
+        return ["You Lose! Paper beats Rock", 0];
 
     } else if (playerSelection === "rock" && computerSelection === "rock") {
-        return "It's a Draw! Nobody wins";
+        return ["It's a Draw! Nobody wins", 2];
 
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You Win! Paper beats Rock";
+        return ["You Win! Paper beats Rock", 1];
 
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You Lose! Scissors beats Paper";
+        return ["You Lose! Scissors beats Paper", 0];
 
     } else if (playerSelection === "paper" && computerSelection === "paper") {
-        return "It's a Draw! Nobody wins";    
+        return ["It's a Draw! Nobody wins", 2];    
 
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You Win! Scissors beats Paper";
+        return ["You Win! Scissors beats Paper", 1];
 
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You Lose! Rock beats Scissors";
+        return ["You Lose! Rock beats Scissors", 0];
 
     } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        return "It's a Draw! Nobody wins";
+        return ["It's a Draw! Nobody wins", 2];
 
     } else {
-        return "Error! You didn't choose Rock, Paper or Scissors";
+        return ["Error! You didn't choose Rock, Paper or Scissors", 2];
     }
 }
 
@@ -53,8 +59,29 @@ function game() {
         // Check what the choice of the computer is
         console.log(computerChoice);
         
-        // Call the playGround function
-        console.log(playRound(userChoice, computerChoice));
+        // Call the playGround function and store the return values in a variable
+        let results = playRound(userChoice, computerChoice);
+        let resultText = results[0];
+        let resultNumber = results[1];
+
+        console.log(resultText);
+
+        if (resultNumber === 1) {
+            userScore++
+        } else if (resultNumber === 0) {
+            computerScore++
+        } else {
+            i--;
+        }
+
+        console.log(userScore);
+
+        if ( i === 4 && userScore > computerScore ) {
+            console.log("You Win!");
+        } else if ( i === 4 && userScore < computerScore ) {
+            console.log("You Lose!");
+        }
+
     }
 
 }
