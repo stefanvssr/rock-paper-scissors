f325967b21f7c72ac15c1def0e197314d8fcb73a
Stefan Visser <me@stefanvssr.dev>
2022-11-20 22:54:14 +0100

    Finish v2.0

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-let computerChoice;
+let playerPoints = 0;
+let computerPoints = 0;
 
-// Scores
-let userScore = 0;
-let computerScore = 0;
+const computerPointsNode = document.querySelector(".computer-points");
+const playerPointsNode = document.querySelector(".player-points");
+const resultTextNode = document.querySelector(".result");
+const resetButton = document.querySelector(".reset");
+const buttons = document.querySelectorAll(".btn-rps");
+
+buttons.forEach( (button) => {
+    button.addEventListener('click', (e) => {
+        let playerChoice = e.target.value;
+        let computerChoice = getComputerChoice();
+        let results = playRound(playerChoice, computerChoice);
+
+        game(playerChoice, computerChoice, results);
+    });
+});
 
 // The computer randomly makes a choice between rock, paper or scissors
 function getComputerChoice() {
@@ -46,40 +59,54 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+function endGame() {
+    if (playerPoints === 5 || computerPoints === 5 ) {
+        buttons.forEach( (button) => {
+            button.disabled = true;
+        });
 
-function game() {
-
-    // Store the input of the user and make the input of the user lowercase
-    let userChoice = prompt("Make your choice: Rock, Paper or Scissor").toLowerCase();
-
-    // Store the selection of the computer
-    computerChoice = getComputerChoice();
-    // Check what the choice of the computer is
-    console.log(computerChoice);
-    
-    // Call the playGround function and store the return values in a variable
-    let results = playRound(userChoice, computerChoice);
-    let resultText = results[0];
-    let resultNumber = results[1];
-
-    console.log(resultText);
-
-    if (resultNumber === 1) {
-        userScore++
-    } else if (resultNumber === 0) {
-        computerScore++
-    } else {
-        i--;
+        resetButton.style.display = "inline-block";
     }
 
-    console.log(userScore);
-
-    if ( i === 4 && userScore > computerScore ) {
-        console.log("You Win!");
-    } else if ( i === 4 && userScore < computerScore ) {
-        console.log("You Lose!");
+    if ( playerPoints === 5 ) {
+        playerPointsNode.textContent = `${playerPoints} (You win!)`;
+    } else if ( computerPoints === 5 ) {
+        playerPointsNode.textContent = `${playerPoints} (You lose!)`;
     }
-
 }
 
-game();
+function resetGame() {
+    resetButton.addEventListener('click', () => {
+        resetButton.style.display = "none";
+
+        playerPoints = 0;
+        computerPoints = 0;
+        playerPointsNode.textContent = 0;
+        computerPointsNode.textContent = 0;
+        resultTextNode.textContent = "";
+
+        buttons.forEach( (button) => {
+            button.disabled = false;
+        });
+
+
+    });
+}
+
+function game(user, computer, result) {
+    let resultText = result[0];
+    let resultNumber = result[1];
+
+    if (resultNumber === 1) {
+        playerPoints++
+    } else if (resultNumber === 0) {
+        computerPoints++
+    }
+
+    resultTextNode.textContent = resultText;
+    playerPointsNode.textContent = playerPoints;
+    computerPointsNode.textContent = computerPoints;
+
+    endGame();
+    resetGame();
+}
